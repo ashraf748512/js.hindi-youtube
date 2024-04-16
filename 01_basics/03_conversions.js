57062b9334e62bb029c7a2c772fcd2d57b76466e
ashraf748512 <123326629+ashraf748512@users.noreply.github.com>
2024-04-16 12:31:58 +0000

    conversion config

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,8 @@
+let score="33"
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof NaN);
+let name="faizan Ashraf"
+/******************OPERATION************** */
+
+console.log(`hello this is ${name} and his roll no is ${score}`);
